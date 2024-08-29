bb69841eb6bd77c927565b2318ea107768d6981a
MadFlasheroo7 <jayeshseth45@gmail.com>
2024-08-29 14:42:11 +0530

    changes: update cats

--- app/src/main/java/pro/jayeshseth/animations/util/CatItem.kt
+++ app/src/main/java/pro/jayeshseth/animations/util/CatItem.kt
@@ -3,5 +3,5 @@ package pro.jayeshseth.animations.util
 data class CatItem(
     val id: Int,
     val catName: String,
-    val catImage: String = "https://source.unsplash.com/random/?cat/$id",
+    val catImage: String = "https://cataas.com/cat",
 )
