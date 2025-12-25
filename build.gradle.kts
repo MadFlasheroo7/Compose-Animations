f6ea4cfcea7f73187a350f3242faa35552869ffe
Madflasheroo7 <jayeshseth45@gmail.com>
2025-12-26 00:11:07 +0530

    - add multiplatform deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,5 +9,6 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kotlinAtomicfu) apply false
 }
 true
